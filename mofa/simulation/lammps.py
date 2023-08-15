265db788862655bacdab63004e4f4ca9cd29bb3d
doublylinkedlist <williamyxl@gmail.com>
2023-08-15 10:08:54 -0500

    Update lammps.py

--- mofa/simulation/lammps.py
+++ mofa/simulation/lammps.py
@@ -1,5 +1,7 @@
 """Simulation operations that involve LAMMPS"""
 import ase
+import io
+from cif2lammps.main_conversion import single_conversion
 
 
 class LAMMPSRunner:
@@ -11,6 +13,18 @@ class LAMMPSRunner:
 
     def __init__(self, lammps_command: str):
         self.lammps_command: str = lammps_command
+
+    def prep_molecular_dynamics(self, mof: cif_str) -> str:
+        """Run a molecular dynamics trajectory
+        Args:
+            mof: Starting structure
+            timesteps: Number of timesteps to run
+            report_frequency: How often to report structures
+        Returns:
+            Structures produced at specified intervals
+        """
+        
+        raise NotImplementedError()
 
     def run_molecular_dynamics(self, mof: ase.Atoms, timesteps: int, report_frequency: int) -> list[ase.Atoms]:
         """Run a molecular dynamics trajectory
