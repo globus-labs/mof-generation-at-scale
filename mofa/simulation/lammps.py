bd12804a6bf51ea92abb2723feb7def9ba1e7491
doublylinkedlist <williamyxl@gmail.com>
2023-08-30 09:34:17 -0500

    Update lammps.py

--- mofa/simulation/lammps.py
+++ mofa/simulation/lammps.py
@@ -18,6 +18,7 @@ class LAMMPSRunner:
     lmp_sims_root_path = "lmp_sims"
     cif_files_root_path = "cif_files"
     cif_files_paths = []
+
     def __init__(self, lammps_command: str = "npt_tri", lmp_sims_root_path: str = "lmp_sims", cif_files_root_path: str = "cif_files"):
         """Read cif files from input directory, make directory for lammps simulation input files
 
