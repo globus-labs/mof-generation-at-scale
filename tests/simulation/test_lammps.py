8ee94bf4aa757fdc1a5fb17b30e51278c39bcace
doublylinkedlist <williamyxl@gmail.com>
2023-09-07 14:35:35 -0500

    Update test_lammps.py

--- tests/simulation/test_lammps.py
+++ tests/simulation/test_lammps.py
@@ -7,7 +7,6 @@ def test_LMPRunner():
                              lmp_sims_root_path="lmp_sims",
                              cif_files_root_path="mofa/simulation/cif_files")
     for test_file in LMPrunner.cif_files_paths:
-        lmp_path = LMPrunner.prep_molecular_dynamics_single(os.path.join(LMPrunner.cif_files_root_path,
-                                                                         test_file),
+        lmp_path = LMPrunner.prep_molecular_dynamics_single(test_file,
                                                             timesteps=1000, report_frequency=100, stepsize_fs=0.5)
         print(lmp_path)
