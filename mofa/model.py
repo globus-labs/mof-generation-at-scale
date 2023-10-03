56ddd1cc4448f473406aec697f3f3cade771f861
Logan Ward <ward.logan.t@gmail.com>
2023-10-03 15:48:23 -0400

    Clarified a few points of the data models

--- mofa/model.py
+++ mofa/model.py
@@ -1,6 +1,7 @@
 """Data models for a MOF class"""
 from dataclasses import dataclass, field
 from functools import cached_property
+from typing import Sequence
 from pathlib import Path
 from io import StringIO
 
@@ -47,6 +48,17 @@ class LigandDescription:
         """All atoms which are not part of a fragment"""
         raise NotImplementedError()
 
+    def generate_template(self, spacing_distance: float | None = None) -> ase.Atoms:
+        """Generate a version of the ligand with only the fragments at the end
+
+        Args:
+            spacing_distance: Distance to enforce between the fragments. Set to ``None``
+                to keep the current distance
+        Returns:
+            The template with the desired spacing
+        """
+        raise NotImplementedError()
+
 
 @dataclass
 class MOFRecord:
@@ -90,7 +102,32 @@ class MOFRecord:
 
         return MOFRecord(structure=Path(cif_path).read_text(), **kwargs)
 
+    def replace_ligand(self, new_ligand: ase.Atoms) -> 'MOFRecord':
+        """Create a new MOF by replacing the ligands in this MOF with new
+
+        Args:
+            new_ligand:
+
+        Returns:
+
+        """
+        return assemble_mof(self.nodes, [new_ligand], self.topology)
+
     @cached_property
     def atoms(self) -> ase.Atoms:
         """The structure as an ASE Atoms object"""
         return next(read_cif(StringIO(self.structure), index=slice(None)))
+
+
+def assemble_mof(nodes: Sequence[NodeDescription], ligands: Sequence[LigandDescription], topology: str) -> MOFRecord:
+    """Generate a new MOF from the description of the nodes, ligands and toplogy
+
+    Args:
+        nodes: Descriptions of each node
+        ligands: Description of the ligands
+        topology: Name of the topology
+
+    Returns:
+        A new MOF record
+    """
+    raise NotImplementedError()
