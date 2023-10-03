67362587186f72de98f38c9ea8f44db42c61d9b1
Logan Ward <ward.logan.t@gmail.com>
2023-10-03 16:33:30 -0400

    Change the interface to pass the whole MOF record
    We need the CIF, which holds the bonding structure

--- mofa/simulation/lammps.py
+++ mofa/simulation/lammps.py
@@ -1,6 +1,8 @@
 """Simulation operations that involve LAMMPS"""
 import ase
 
+from mofa.model import MOFRecord
+
 
 class LAMMPSRunner:
     """Interface for running pre-defined LAMMPS workflows
@@ -12,11 +14,11 @@ class LAMMPSRunner:
     def __init__(self, lammps_command: str):
         self.lammps_command: str = lammps_command
 
-    def run_molecular_dynamics(self, mof: ase.Atoms, timesteps: int, report_frequency: int) -> list[ase.Atoms]:
+    def run_molecular_dynamics(self, mof: MOFRecord, timesteps: int, report_frequency: int) -> list[ase.Atoms]:
         """Run a molecular dynamics trajectory
 
         Args:
-            mof: Starting structure
+            mof: Record describing the MOF. Includes the structure in CIF format, which includes the bonding information used by UFF
             timesteps: Number of timesteps to run
             report_frequency: How often to report structures
         Returns:
