e095d5abdbda8de7b82d2ce97d572fe818c7ace4
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-10-11 15:14:35 -0500

    Update difflinker_sample.py

--- mofa/difflinker_sample.py
+++ mofa/difflinker_sample.py
@@ -1,23 +1,29 @@
 import os
 import argparse
 import subprocess
-from utils.difflinker_sample_and_analyze import main_run, run_dflk_sample_analyze
+from mofa.utils.difflinker_sample_and_analyze import main_run, run_dflk_sample_analyze
+from typing import *
 
-nodes = ['CuCu']
-# change to the line below to reproduce paper result
-# nodes = [i.split('_')[1].split('.sdf')[0] for i in os.listdir('data/conformers') if 'conformers' in i]
-
-for n_atoms in [8]:
-# change to the line below to reproduce paper result
-# for n_atoms in range(5,11):
-    print(f'Sampling {n_atoms} atoms...')
-    for node in nodes:
-        if node != 'V':
-            print(f'Now on node: {node}')
-            OUTPUT_DIR = f'output/n_atoms_{n_atoms}/{node}'
-            os.makedirs(OUTPUT_DIR,exist_ok=True)
-            # subprocess.run(f'python -W ignore utils/difflinker_sample_and_analyze.py --linker_size {n_atoms} --fragments data/fragments_all/{node}/hMOF_frag.sdf --model models/geom_difflinker.ckpt --output {OUTPUT_DIR} --n_samples 1',shell=True)
-            main_run(input_path=f"data/fragments_all/{node}/hMOF_frag_frag.sdf", model="models/geom_difflinker.ckpt", linker_size=str(n_atoms), output_dir=OUTPUT_DIR, n_samples=1, n_steps=None, anchors=None)
-
-            # change to the line below to reproduce paper result
-            #subprocess.run(f'python -W ignore utils/difflinker_sample_and_analyze.py --linker_size {n_atoms} --fragments data/fragments_all/{node}/hMOF_frag.sdf --model models/geom_difflinker.ckpt --output {OUTPUT_DIR} --n_samples 20',shell=True)
+def sampler(nodes: List[str]=['CuCu'], n_atoms_list: List[int]=[8]):
+    # nodes = ['CuCu']
+    # change to the line below to reproduce paper result
+    # nodes = [i.split('_')[1].split('.sdf')[0] for i in os.listdir('data/conformers') if 'conformers' in i]
+    
+    for n_atoms in n_atoms_list:
+    # change to the line below to reproduce paper result
+    # for n_atoms in range(5,11):
+        print(f'Sampling {n_atoms} atoms...')
+        for node in nodes:
+            if node != 'V':
+                print(f'Now on node: {node}')
+                OUTPUT_DIR = f'output/n_atoms_{n_atoms}/{node}'
+                os.makedirs(OUTPUT_DIR,exist_ok=True)
+                # subprocess.run(f'python -W ignore utils/difflinker_sample_and_analyze.py --linker_size {n_atoms} --fragments data/fragments_all/{node}/hMOF_frag.sdf --model models/geom_difflinker.ckpt --output {OUTPUT_DIR} --n_samples 1',shell=True)
+                main_run(input_path=f"data/fragments_all/{node}/hMOF_frag_frag.sdf", model="models/geom_difflinker.ckpt", linker_size=str(n_atoms), output_dir=OUTPUT_DIR, n_samples=1, n_steps=None, anchors=None)
+    
+                # change to the line below to reproduce paper result
+                #subprocess.run(f'python -W ignore utils/difflinker_sample_and_analyze.py --linker_size {n_atoms} --fragments data/fragments_all/{node}/hMOF_frag.sdf --model models/geom_difflinker.ckpt --output {OUTPUT_DIR} --n_samples 20',shell=True)
+if __name__ == "__main__":
+    nodes = ['CuCu']
+    n_atoms_list = [8]
+    sampler(nodes, n_atoms_list)
