6a4844c595c0eb1b5b7f6ca9a69faf6dc3a9bcf5
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-10-17 23:51:35 -0500

    Update fragment.py

--- mofa/fragment.py
+++ mofa/fragment.py
@@ -5,4 +5,19 @@ import ase
 
 from model import MOFRecord
 
+def fragment_mof_linkers(
+        starting_model: str | Path,
+        examples: list[MOFRecord],
+        num_epochs: int
+) -> Path:
+    """Fragment linkers of MOFs
+
+    Args:
+        starting_model: Path to the starting weights of the model
+        examples: Seed examples of linkers (data model TBD)
+        num_epochs: Number of training epochs
+    Returns:
+        Path to the new model weights
+    """
+    raise NotImplementedError()
 
