78fb138176c605df6feb2151ec661a5f6d0cf3d5
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-10-17 23:50:45 -0500

    Update fragment.py

--- mofa/fragment.py
+++ mofa/fragment.py
@@ -1,1 +1,8 @@
+"""Functions pertaining to fragmenting MOF linkers to generate data for generative model training and sampling"""
+from pathlib import Path
+
+import ase
+
+from model import MOFRecord
+
 
