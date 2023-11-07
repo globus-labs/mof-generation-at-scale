602df60fe86e1718b04a9eb395cadf2528979b07
Logan Ward <ward.logan.t@gmail.com>
2023-11-07 13:23:32 -0500

    Don't discard errors in test

--- tests/simulation/test_lammps.py
+++ tests/simulation/test_lammps.py
@@ -9,11 +9,7 @@ def test_lammps_runner(cif_files):
     )
 
     for test_file in cif_files.glob("*.cif"):
-        try:
-            lmp_path = lmprunner.prep_molecular_dynamics_single(test_file,
-                                                                timesteps=1000,
-                                                                report_frequency=100,
-                                                                stepsize_fs=0.5)
-            logging.info(lmp_path)
-        except Exception as e:
-            logging.error(e)
+        lmp_path = lmprunner.prep_molecular_dynamics_single(test_file,
+                                                            timesteps=1000,
+                                                            report_frequency=100,
+                                                            stepsize_fs=0.5)
