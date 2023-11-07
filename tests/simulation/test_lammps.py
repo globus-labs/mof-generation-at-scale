221aed9ae2460323c5d71dc0b5ef9d4b8fb91e35
Logan Ward <ward.logan.t@gmail.com>
2023-11-07 13:38:47 -0500

    Print the identity of the file that fails

--- tests/simulation/test_lammps.py
+++ tests/simulation/test_lammps.py
@@ -9,8 +9,11 @@ def test_lammps_runner(cif_files):
     )
 
     for test_file in cif_files.glob("*.cif"):
-        lmp_path = lmprunner.prep_molecular_dynamics_single(test_file,
-                                                            timesteps=1000,
-                                                            report_frequency=100,
-                                                            stepsize_fs=0.5)
+        try:
+            lmp_path = lmprunner.prep_molecular_dynamics_single(test_file,
+                                                                timesteps=1000,
+                                                                report_frequency=100,
+                                                                stepsize_fs=0.5)
+        except Exception as e:
+            raise ValueError(f'Failure for {test_file}') from e
         assert Path(lmp_path).exists()
