f0917d66f75f05e10a6a074f5c7301e460ae8a95
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-11-08 08:44:49 -0600

    Update test_generator.py

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -4,7 +4,7 @@ from mofa.generator import train_generator, run_generator
 import numpy as np
 import torch
 from mofa.utils.src.lightning import DDPM
-
+from mofa.utils.difflinker_sample_and_analyze import main_run
 
 @fixture()
 def load_model():
@@ -12,6 +12,10 @@ def load_model():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     ddpm = DDPM.load_from_checkpoint(model, map_location=device).eval().to(device)
     return ddpm
+
+def test_load_model():
+    model = load_model
+    print("Successful?")
 
 def test_training():
     ...
