80e4f7962facd0bf3a7eda9475e9671239876af0
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-11-08 08:32:30 -0600

    Update test_generator.py

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -2,10 +2,14 @@ import pytest
 from pytest import fixture
 from mofa.generator import train_generator, run_generator
 import numpy as np
+import torch
+from 
 
 @fixture()
 def load_model():
-    ...
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    ddpm = DDPM.load_from_checkpoint(model, map_location=device).eval().to(device)
+    return ddpm
 
 def test_training():
     ...
