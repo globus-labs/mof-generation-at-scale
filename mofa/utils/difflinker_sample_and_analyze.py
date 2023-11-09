3e0d95bb6d8b5eb2f220eefb4706580a088e5b70
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-11-08 22:37:26 -0600

    Update difflinker_sample_and_analyze.py

--- mofa/utils/difflinker_sample_and_analyze.py
+++ mofa/utils/difflinker_sample_and_analyze.py
@@ -7,11 +7,11 @@ from tqdm import tqdm
 import torch
 from rdkit import Chem
 
-from utils.src import const
-from utils.src.datasets import collate_with_fragment_edges, get_dataloader, parse_molecule
-from utils.src.lightning import DDPM
-from utils.src.linker_size_lightning import SizeClassifier
-from utils.src.visualizer import save_xyz_file, visualize_chain
+from mofa.utils.src import const
+from mofa.utils.src.datasets import collate_with_fragment_edges, get_dataloader, parse_molecule
+from mofa.utils.src.lightning import DDPM
+from mofa.utils.src.linker_size_lightning import SizeClassifier
+from mofa.utils.src.visualizer import save_xyz_file, visualize_chain
 from tqdm import tqdm
 
 
