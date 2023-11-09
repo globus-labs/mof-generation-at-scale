74a3ba941f4b7583ccc2f10cb0500917ef63b9cf
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-11-08 22:20:09 -0600

    Update test_generator.py

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -13,8 +13,8 @@ def load_model():
     ddpm = DDPM.load_from_checkpoint(model, map_location=device).eval().to(device)
     return ddpm
 
-def test_load_model():
-    model = load_model
+def test_load_model(load_model):
+    print(load_model.__class__.__name__)
     print("Successful?")
 
 def test_training():
