309f37eac232283e4863f26d45d0313d33d87fab
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-11-14 12:46:43 -0600

    Update test_generator.py

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -4,14 +4,22 @@ from mofa.generator import train_generator, run_generator
 import numpy as np
 import torch
 from mofa.utils.src.lightning import DDPM
+from mofa.utils.src.linkzer_size_lightning import SizeGNN
 from mofa.utils.difflinker_sample_and_analyze import main_run
 
 def test_cuda():
     assert torch.cuda.is_available()
 
 @fixture()
-def load_model():
+def load_denoising_model():
     model = "mofa/models/geom_difflinker.ckpt"
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    ddpm = DDPM.load_from_checkpoint(model, map_location=device).eval().to(device)
+    return ddpm
+
+@fixture()
+def load_size_gnn_model():
+    model = "mofa/models/geom_size_gnn.ckpt"
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     ddpm = DDPM.load_from_checkpoint(model, map_location=device).eval().to(device)
     return ddpm
