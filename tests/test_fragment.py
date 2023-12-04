6d89d863f48372910e7daccd16d12725847ead4f
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-12-03 23:32:33 -0600

    Update test_fragment.py

--- tests/test_fragment.py
+++ tests/test_fragment.py
@@ -1,1 +1,12 @@
+@mark.parametrize('n_atoms', [3])
+@mark.parametrize('node', ['CuCu', 'ZnZn', 'ZnOZnZnZn'])
+@mark.parametrize('n_samples', [1, 3])
+def test_sampling_num_atoms(n_atoms, node, n_samples):
+    run_generator(n_atoms=n_atoms, node=node, n_samples=n_samples)
+
+@mark.parametrize('nodes', [['CuCu'], ['CuCu', 'ZnZn']])
+def test_fragmentation(nodes):
+    # fragmentation(nodes)
+    # process_fragments(nodes)
+    fragment_mof_linkers(nodes)
 
