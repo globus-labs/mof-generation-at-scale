b101c4b15e6d78a0ec2153e0c124236c1a94762c
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-12-03 23:55:12 -0600

    Update frag_utils.py

--- mofa/utils/frag_utils.py
+++ mofa/utils/frag_utils.py
@@ -20,7 +20,7 @@ from rdkit.Chem import MolStandardize
 
 import matplotlib.pyplot as plt
 
-import utils.sascorer as sascorer
+import mofa.utils.sascorer as sascorer
 from itertools import chain, product
 
 from joblib import Parallel, delayed
