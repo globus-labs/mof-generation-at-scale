588e5961283586b7d77943a2e8ecf9716ab45f31
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-12-04 00:00:14 -0600

    Update test_fragment.py

--- tests/test_fragment.py
+++ tests/test_fragment.py
@@ -6,12 +6,6 @@ from mofa.fragment import fragment_mof_linkers
 from mofa.generator import train_generator, run_generator
 import os
 
-@mark.parametrize('n_atoms', [3])
-@mark.parametrize('node', ['CuCu', 'ZnZn', 'ZnOZnZnZn'])
-@mark.parametrize('n_samples', [1, 3])
-def test_sampling_num_atoms(n_atoms, node, n_samples):
-    run_generator(n_atoms=n_atoms, node=node, n_samples=n_samples)
-
 @mark.parametrize('nodes', [['CuCu'], ['CuCu', 'ZnZn']])
 def test_fragmentation(nodes):
     # fragmentation(nodes)
