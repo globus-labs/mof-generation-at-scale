dbf9f4d7d5d323e27acf39ffa90aba625fdb3556
Logan Ward <WardLT@users.noreply.github.com>
2023-12-08 11:12:35 -0500

    Add code for scoring lattice strain (#30)
    * Add code for scoring lattice strain
    
    * Check math against bilbao
    
    (I was wrong)

--- mofa/scoring/geometry.py
+++ mofa/scoring/geometry.py
@@ -1,8 +1,13 @@
 """Metrics for screening a MOF or linker based on its geometry"""
+from io import StringIO
+
 import numpy as np
 import ase
+from ase.io.vasp import read_vasp
 
-from mofa.scoring.base import MOFScorer
+from mofa.model import MOFRecord
+from dataclasses import dataclass
+from mofa.scoring.base import MOFScorer, Scorer
 
 
 class MinimumDistance(MOFScorer):
@@ -12,3 +17,28 @@ class MinimumDistance(MOFScorer):
         dists: np.ndarray = linker.get_all_distances(mic=True)
         inds = np.triu_indices(dists.shape[0], k=1)
         return np.min(dists[inds])
+
+
+@dataclass
+class LatticeParameterChange(Scorer):
+    """Score the stability of a MOF based on the lattice parameter change"""
+
+    md_level: str = 'uff'
+    """Level of accuracy used for the molecular dynamics simulation"""
+
+    def score_mof(self, record: MOFRecord) -> float:
+        # Get the trajectory from the record
+        if self.md_level not in record.md_trajectory:
+            raise ValueError(f'No data available for MD simulations at level: "{self.md_level}"')
+        traj = record.md_trajectory[self.md_level]
+
+        # Get the initial and final structures
+        init_strc = read_vasp(StringIO(traj[0]))
+        final_strc: ase.Atoms = read_vasp(StringIO(traj[1]))
+
+        # Compute the maximum principal strain
+        #  Following: https://www.cryst.ehu.es/cgi-bin/cryst/programs/nph-strain
+        strain = np.matmul(init_strc.cell.array, np.linalg.inv(final_strc.cell.array)) - np.eye(3)
+        strain = 0.5 * (strain + strain.T)
+        strains = np.linalg.eigvals(strain)
+        return np.abs(strains).max()
