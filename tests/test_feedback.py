06bcc708dc1ce021c8676f602c29d7d2f54cc853
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-12-19 01:14:03 -0600

    Update test_feedback.py

--- tests/test_feedback.py
+++ tests/test_feedback.py
@@ -10,6 +10,6 @@ from mofa.feedback import collect_hp_linkers, ROOT_DIR
 def node_name():
     return "TEMPNODE"
 
-@mark.paramaterize(["input_path", "output_path"], [ (Path(os.path.join(ROOT_DIR, "lammps/pdb/ligands")), Path(os.path.join(ROOT_DIR, "feedback_output"))), ])
+@mark.paramaterize("input_path", "output_path", [ (Path(os.path.join(ROOT_DIR, "lammps/pdb/ligands")), Path(os.path.join(ROOT_DIR, "feedback_output"))), ])
 def test_feedback(input_path, output_path, node_name):
     collect_hp_linkers()
