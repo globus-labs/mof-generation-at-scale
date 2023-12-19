1f873b87c382495c9b06b6adeed7979d25561753
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-12-19 01:04:51 -0600

    Update test_feedback.py

--- tests/test_feedback.py
+++ tests/test_feedback.py
@@ -6,7 +6,8 @@ import torch
 from mofa.feedback import collect_hp_linkers
 
 @fixture
-
+def node_name():
+    return "TEMPNODE"
 
 def test_feedback(input_path, output_path, node_name):
     collect_hp_linkers()
