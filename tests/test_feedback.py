ffe9103afbd0e0bae2aa8b9e9db64c822ba40ee8
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-12-19 01:06:49 -0600

    Update test_feedback.py

--- tests/test_feedback.py
+++ tests/test_feedback.py
@@ -3,11 +3,12 @@ from pathlib import Path
 from pytest import fixture, mark
 import torch
 
-from mofa.feedback import collect_hp_linkers
+from mofa.feedback import collect_hp_linkers, ROOT
 
 @fixture
 def node_name():
     return "TEMPNODE"
 
+@mark.paramaterize(["input_path", "output_path"], [])
 def test_feedback(input_path, output_path, node_name):
     collect_hp_linkers()
