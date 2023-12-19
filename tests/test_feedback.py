f9846edc4c0465ded72886104b2937d9be2a2eea
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-12-19 01:03:40 -0600

    Update test_feedback.py

--- tests/test_feedback.py
+++ tests/test_feedback.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from pytest import fixture, mark
 import torch
 
-from mofa.feedback import 
+from mofa.feedback import collect_hp_linkers
 
 def test_feedback():
-    ...
+    collect_hp_linkers()
