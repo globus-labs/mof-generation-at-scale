1a632d84d2833c7d898331aea2e8d350441227fc
Logan Ward <ward.logan.t@gmail.com>
2024-01-17 13:18:51 -0500

    Introduce a method for detecting valancy given bond length

--- mofa/utils/xyz.py
+++ mofa/utils/xyz.py
@@ -3,7 +3,9 @@ from threading import Lock
 
 from rdkit.Chem import rdDetermineBonds, AllChem
 from rdkit import Chem
+import numpy as np
 
+from mofa.utils.src import const
 
 _generate_lock = Lock()
 
@@ -19,7 +21,7 @@ def xyz_to_mol(xyz: str) -> Chem.Mol:
 
     mol = Chem.MolFromXYZBlock(xyz)
     rdDetermineBonds.DetermineConnectivity(mol)
-    rdDetermineBonds.DetermineBonds(mol)
+    rdDetermineBonds.DetermineBondOrders(mol)
     return mol
 
 
@@ -58,3 +60,48 @@ def smiles_to_xyz(smiles: str) -> str:
         # Save the conformer to an XYZ file
         xyz = Chem.MolToXYZBlock(mol)
         return xyz
+
+
+def unsaturated_xyz_to_mol(xyz: str) -> Chem.Mol:
+    """"""
+
+    # First determine connectivity given 3D coordinates
+    mol: Chem.Mol = Chem.MolFromXYZBlock(xyz)
+    rdDetermineBonds.DetermineConnectivity(mol)
+    conformer: Chem.Conformer = mol.GetConformer(0)
+    positions = conformer.GetPositions()
+
+    # Based on that connectivity, infer the bond order
+    for bond in mol.GetBonds():
+        bond: Chem.Bond = bond
+
+        # Get the distance between atoms
+        atom_1, atom_2 = bond.GetBeginAtom(), bond.GetEndAtom()
+        atom_1: Chem.Atom = atom_1
+        distance = np.linalg.norm(
+            positions[atom_1.GetIdx(), :] - positions[atom_2.GetIdx(), :]
+        ) * 100  # Distance in pm, to match with the database
+
+        # Infer if the bond order is larger than single
+        # Adapted from "utils/src/molecule_builder.py
+        type_1, type_2 = atom_1.GetSymbol(), atom_2.GetSymbol()
+        margins = const.MARGINS_EDM
+        bond_type = Chem.BondType.SINGLE
+
+        if type_1 in const.BONDS_2 and type_2 in const.BONDS_2[type_1]:
+            thr_bond2 = const.BONDS_2[type_1][type_2] + margins[1]
+            if distance < thr_bond2:
+                bond_type = Chem.BondType.DOUBLE
+                if type_1 in const.BONDS_3 and type_2 in const.BONDS_3[type_1]:
+                    thr_bond3 = const.BONDS_3[type_1][type_2] + margins[2]
+                    if distance < thr_bond3:
+                        bond_type = Chem.BondType.TRIPLE
+        bond.SetBondType(bond_type)
+
+    # Add hydrogens to the molecule
+    mol.UpdatePropertyCache()  # Detects the valency
+    Chem.AddHs(mol, explicitOnly=True, addCoords=True)
+
+    # TODO (wardlt): Generate positions for the hydrogen atoms, probably by just re-generating the whole conformer
+
+    return mol
--- tests/test_xyz.py
+++ tests/test_xyz.py
@@ -3,7 +3,7 @@
 from rdkit import Chem
 from pytest import mark
 
-from mofa.utils.xyz import smiles_to_xyz, xyz_to_smiles
+from mofa.utils.xyz import smiles_to_xyz, xyz_to_smiles, unsaturated_xyz_to_mol
 
 
 @mark.parametrize('smiles', ['C', 'C=C', 'c1cnccc1'])
@@ -17,3 +17,25 @@ def test_reversibility(smiles):
     start_inchi = Chem.MolToInchi(Chem.MolFromSmiles(smiles))
     end_inchi = Chem.MolToInchi(Chem.MolFromSmiles(new_smiles))
     assert start_inchi == end_inchi
+
+
+@mark.parametrize('smiles', ['C', 'C=C', 'c1cnccc1'])
+def test_from_unsaturated(smiles):
+    """Test whether we can infer smiles from unsaturated"""
+
+    # Create an XYZ w/o hydrogens
+    xyz = smiles_to_xyz(smiles).rstrip().split("\n")
+    non_hs = [i for i in xyz[2:] if not i.startswith("H")]
+    non_h_xyz = f"{len(non_hs)}\n\n" + "\n".join(non_hs)
+
+    # Try to re-determine the XYZ structure
+    mol = unsaturated_xyz_to_mol(non_h_xyz)
+
+    # See if the InChI matches
+    start_inchi = Chem.MolToInchi(Chem.MolFromSmiles(smiles))
+    end_inchi = Chem.MolToInchi(mol)
+    assert start_inchi == end_inchi, f'In: {smiles} - Out: {Chem.MolToSmiles(mol)}'
+
+    # Get the implicit valence
+    new_xyz = Chem.MolToXYZBlock(mol)
+    assert len(xyz) == len(new_xyz.split("\n")), 'Size of the XYZ files are different'
