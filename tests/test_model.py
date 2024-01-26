f04020d6a7929a5d701ccb878f14c7698f18a64c
doublylinkedlist <williamyxl@gmail.com>
2024-01-26 04:03:21 -0600

    Update test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,10 +2,12 @@ from math import isclose
 
 from pytest import mark
 import numpy as np
+import pandas as pd
+import io
 
 from mofa.model import MOFRecord, LigandTemplate, LigandDescription
 from mofa.utils.conversions import read_from_string
-
+from rdkit import Chem
 
 def test_create(example_cif):
     mof = MOFRecord.from_file(example_cif, identifiers={'local': 'test'})
@@ -40,16 +42,28 @@ def test_ligand_model(file_path):
     assert ligand.anchor_atoms == [[0, 1, 2], [3, 4, 5]]
     assert ligand.dummy_element == template.dummy_element
 
-@mark.parametrize('anchor_type', ['COO', 'cyano'])
+@mark.parametrize('anchor_type', ['COO'])
 def test_ligand_description_H_inference(file_path, anchor_type):
     desc = LigandDescription.from_yaml(file_path / 'difflinker' / 'templates' / f'description_{anchor_type}.yml')
-    orig_xyz_str = desc.xyz
     desc.infer_H_and_bond_safe()
-    new_xyz_str = desc.xyz
+    needed_orig_xyz_str = "\n".join(desc.xyz.split("\n")[2:])
+    needed_new_xyz_str = "\n".join(desc.xyz_H.split("\n")[2:])
+    xyz_diff = needed_new_xyz_str.replace("needed_orig_xyz_str", "")
+    df = pd.read_csv(io.StringIO(xyz_diff), sep=r"\s+", header=None, index_col=None, names=["element", "x", "y", "z"])
 
-    # Test if the Hs are added and no heavy atom information has been modified
-    with_dummies = desc.replace_with_dummy_atoms()
-    assert with_dummies.symbols.count(desc.dummy_element) == 2
+    # test if all added atoms are Hs
+    all_added_atoms_are_Hs = np.all(df["element"].to_numpy() == "H")
+
+    # test if none of the Hs are added to the anchor atoms
+    rdmol = Chem.rdmolfiles.MolFromMolBlock(mol.write(format='sdf', filename=None))
+    H_is_detected_on_an_anchor = False
+    for x in list(itertools.chain(*ld.anchor_atoms)):
+        rdatom = rdmol.GetAtomWithIdx(x)
+        nbrs = [x.GetSymbol() for x in list(rdatom.GetNeighbors())]
+        if "H" in nbrs:
+            H_is_detected_on_an_anchor = True
+            break
+    assert (not H_is_detected_on_an_anchor) and all_added_atoms_are_Hs
 
 @mark.parametrize('anchor_type', ['COO', 'cyano'])
 def test_ligand_description(file_path, anchor_type):
