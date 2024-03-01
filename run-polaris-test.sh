bcd3e1dc21c166084eed789cb93efc652c05cc3f
Logan Ward <ward.logan.t@gmail.com>
2024-03-01 17:03:47 +0000

    Increase the scale of the test

--- run-polaris-test.sh
+++ run-polaris-test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -le
-#PBS -l select=2:system=polaris
+#PBS -l select=10:system=polaris
 #PBS -l walltime=01:00:00
 #PBS -l filesystems=home:grand:eagle
-#PBS -q debug
+#PBS -q debug-scaling
 #PBS -N test-run
 #PBS -A examol
 
@@ -17,7 +17,7 @@ python run_parallel_workflow.py \
       --node-path input-files/zn-paddle-pillar/node.json \
       --generator-path input-files/zn-paddle-pillar/geom_difflinker.ckpt \
       --ligand-templates input-files/zn-paddle-pillar/template_*.yml \
-      --num-samples 32 \
-      --simulation-budget 8 \
+      --num-samples 512 \
+      --simulation-budget 256 \
       --compute-config polaris
 
