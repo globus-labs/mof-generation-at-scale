7147e835d70c93477fb21b28d6a5850b0a2e3732
Logan Ward <ward.logan.t@gmail.com>
2024-03-16 08:11:24 -0400

    Add MongoDB module

---
+++ tests/test_db.py
@@ -0,0 +1,42 @@
+from pytest import fixture
+from mongomock import MongoClient
+
+from mofa.db import initialize_database, create_records, get_records, update_records, count_records, get_all_records
+from mofa.model import MOFRecord
+
+
+@fixture()
+def coll():
+    # Make the database
+    client = MongoClient()
+    return initialize_database(client)
+
+
+def test_initialize(coll):
+    assert count_records(coll) == 0
+
+
+def test_insert_and_find(coll, example_record):
+    create_records(coll, [example_record])
+    assert count_records(coll) == 1
+
+    copies = get_records(coll, [example_record.name])
+    assert len(copies) == 1
+    assert copies[0].name == example_record.name
+    assert (copies[0].times['created'] - example_record.times['created']).total_seconds() < 1e-6
+
+
+def test_update(coll, example_record):
+    create_records(coll, [example_record])
+
+    example_record.gas_storage['co2'] = (1., 1.)
+    update_records(coll, [example_record])
+
+    copies = get_records(coll, [example_record.name])
+    assert len(copies) == 1
+    assert copies[0].gas_storage['co2'] == [1., 1.]
+
+
+def test_get_all(coll):
+    create_records(coll, [MOFRecord(name=str(x)) for x in range(5)])
+    assert len(list(get_all_records(coll))) == 5
