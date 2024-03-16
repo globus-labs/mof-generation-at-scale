d602f17dfb61fdd7ec480f41f238d4e05b09718c
Logan Ward <ward.logan.t@gmail.com>
2024-03-16 12:11:59 +0000

    Make settings in run script more realistic

--- run-polaris-test.sh
+++ run-polaris-test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -le
-#PBS -l select=2:system=polaris
+#PBS -l select=10:system=polaris
 #PBS -l walltime=01:00:00
 #PBS -l filesystems=home:grand:eagle
-#PBS -q debug
+#PBS -q debug-scaling
 #PBS -N test-run
 #PBS -A examol
 
@@ -22,8 +22,11 @@ python run_parallel_workflow.py \
       --node-path input-files/zn-paddle-pillar/node.json \
       --generator-path input-files/zn-paddle-pillar/geom_difflinker.ckpt \
       --ligand-templates input-files/zn-paddle-pillar/template_*.yml \
-      --num-samples 512 \
+      --num-samples 1024 \
+      --gen-batch-size 16 \
       --simulation-budget 256 \
+      --md-timesteps 1000000 \
+      --md-snapshots 10 \
       --compute-config polaris
 echo Python done
 
