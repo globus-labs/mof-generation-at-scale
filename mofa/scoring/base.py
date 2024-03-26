792f60daf976ea00ac130ad8400cb8afcf36931c
doublylinkedlist <williamyxl@gmail.com>
2024-03-26 10:09:51 -0500

    Update base.py

--- mofa/scoring/base.py
+++ mofa/scoring/base.py
@@ -1,8 +1,10 @@
 """Interfaces for functions which produce scores on either the full MOF or just the geometry of the linker"""
 
 import ase
-
 from mofa.model import MOFRecord
+import io
+import numpy as np
+import pandas as pd
 
 
 class Scorer:
@@ -51,3 +53,27 @@ class MOFScorer(Scorer):
 
     def score_mof(self, record: MOFRecord) -> float:
         return self(record.atoms)
+
+# some scripts to tell if a mof is stable
+__threshold__ = 0.05  # 5 percent
+df = pd.read_json("mofs-25Mar24.json.gz", lines=True)
+for i in df.index:
+    mof_name = df.at[i, "name"]
+    traj_list = df.at[i, "md_trajectory"]["uff"]
+    M = [np.loadtxt(io.StringIO("\n".join(list(filter(None, traj_list[x].strip().split("\n")))[2:5]))) for x in range(0, len(traj_list))]
+    M = np.array(M)
+    A = M[:, 0, :]
+    B = M[:, 1, :]
+    C = M[:, 2, :]
+    a = np.linalg.norm(A, axis=1)
+    b = np.linalg.norm(B, axis=1)
+    c = np.linalg.norm(C, axis=1)
+    alpha = np.arccos(np.sum(B * C, axis=1) / b / c) * 180 / np.pi
+    beta = np.arccos(np.sum(A * C, axis=1) / a / c) * 180 / np.pi
+    gamma = np.arccos(np.sum(A * B, axis=1) / a / b) * 180 / np.pi
+    print(mof_name, np.all((a.mean() - a) / a < __threshold__) & \
+            np.all((b.mean() - b) / b < __threshold__) & \
+            np.all((c.mean() - c) / c < __threshold__) & \
+            np.all((alpha.mean() - alpha) / alpha < __threshold__) & \
+            np.all((beta.mean() - beta) / beta < __threshold__) & \
+            np.all((gamma.mean() - gamma) / gamma < __threshold__))
