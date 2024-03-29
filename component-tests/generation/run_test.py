871241f572f8c6c2468040b00f563e134f17a411
Logan Ward <ward.logan.t@gmail.com>
2024-03-29 14:35:59 +0000

    Add sunspot configuration

--- component-tests/generation/run_test.py
+++ component-tests/generation/run_test.py
@@ -37,13 +37,13 @@ def test_function(model_path: Path, n_atoms: int, template: Path, n_samples: int
     # Run
     template = LigandTemplate.from_yaml(template)
     start_time = perf_counter()
-    run_generator(
+    result = list(run_generator(
         model=model_path,
         templates=[template],
         n_atoms=n_atoms,
         n_samples=n_samples,
         device=device
-    )
+    ))
     run_time = perf_counter() - start_time
 
     return run_time
@@ -64,8 +64,6 @@ if __name__ == "__main__":
     if args.config == "local":
         config = Config(executors=[HighThroughputExecutor(max_workers=1, cpu_affinity='block')])
     elif args.config == "polaris":
-        lammps_cmd = ('/lus/eagle/projects/ExaMol/mofa/lammps-2Aug2023/src/lmp_polaris_nvhpc_kokkos '
-                      '-k on g 1 -sf kk -pk kokkos neigh half neigh/qeq full newton on ').split()
         config = Config(retries=1, executors=[
             HighThroughputExecutor(
                 max_workers=4,
@@ -97,6 +95,54 @@ hostname
                 )
             )
         ])
+    elif args.config == "sunspot":
+        accel_ids = [
+           f"{gid}.{tid}"
+           for gid in range(6)
+           for tid in range(2)
+        ]
+        config = Config(
+            retries=2,
+            executors=[
+                HighThroughputExecutor(
+                    label="sunspot_test",
+                    available_accelerators=accel_ids,  # Ensures one worker per accelerator
+                    cpu_affinity="block",  # Assigns cpus in sequential order
+                    prefetch_capacity=0,
+                    max_workers=12,
+                    cores_per_worker=16,
+                    provider=PBSProProvider(
+                        account="CSC249ADCD08_CNDA",
+                        queue="workq",
+                        worker_init="""
+source activate /lus/gila/projects/CSC249ADCD08_CNDA/mof-generation-at-scale/env
+module reset
+module use /soft/modulefiles/
+module use /home/ftartagl/graphics-compute-runtime/modulefiles
+module load oneapi/release/2023.12.15.001
+module load intel_compute_runtime/release/775.20
+module load gcc/12.2.0
+module list
+
+
+cd $PBS_O_WORKDIR
+pwd
+which python
+hostname
+                        """,
+                        walltime="1:10:00",
+                        launcher=MpiExecLauncher(
+                            bind_cmd="--cpu-bind", overrides="--depth=208 --ppn 1"
+                        ),  # Ensures 1 manger per node and allows it to divide work among all 208 threads
+                        select_options="system=sunspot,place=scatter",
+                        nodes_per_block=1,
+                        min_blocks=0,
+                        max_blocks=1, # Can increase more to have more parallel batch jobs
+                        cpus_per_node=208,
+                    ),
+                ),
+            ]
+        )
     else:
         raise ValueError(f'Configuration not defined: {args.config}')
 
