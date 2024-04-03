a704649c3b1c8c9f5df219e2d22a295cd3813a45
doublylinkedlist <williamyxl@gmail.com>
2024-04-03 14:42:18 -0500

    Update test_cp2k.py

--- tests/simulation/test_cp2k.py
+++ tests/simulation/test_cp2k.py
@@ -8,7 +8,8 @@ from mofa.simulation.cp2k import CP2KRunner
 def test_cp2k_runner(cif_name, cif_dir, tmpdir):
     # Make a CP2k simulator that reads and writes to a temporary directory
     runner = CP2KRunner(
-        cp2k_invocation=tmpdir / "cp2k_sims",
+        cp2k_invocation="cp2k_shell.psmp",
+        run_ddec=True,
     )
 
     test_file = cif_dir / f'{cif_name}.cif'
