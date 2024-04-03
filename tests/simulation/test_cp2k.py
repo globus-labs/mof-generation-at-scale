b7f7091d5213006d4d5e56509ec5dc165111416a
doublylinkedlist <williamyxl@gmail.com>
2024-04-03 02:19:26 -0500

    Update test_cp2k.py

--- tests/simulation/test_cp2k.py
+++ tests/simulation/test_cp2k.py
@@ -5,7 +5,7 @@ from mofa.simulation.cp2k import CP2KRunner
 
 
 @mark.parametrize('cif_name', ['hMOF-0'])
-def test_lammps_runner(cif_name, cif_dir, tmpdir):
+def test_cp2k_runner(cif_name, cif_dir, tmpdir):
     # Make a LAMMPS simulator that reads and writes to a
     runner = CP2KRunner(
         cp2k_invocation=tmpdir / "cp2k_sims",
