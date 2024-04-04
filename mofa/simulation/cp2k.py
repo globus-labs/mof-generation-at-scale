f2e2b41458f6e1e33938f99e5af419b995148551
Logan Ward <WardLT@users.noreply.github.com>
2024-04-04 17:31:56 -0400

    Add interface for CP2K computations (#73)
    * Update environment-cuda117.yml
    
    * Add CP2K harness
    
    * Update environment-cuda117.yml
    
    * Update cp2k-pbe-template.inp
    
    * Update cp2k-pbe-template.inp
    
    * Update cp2k-pbe-template.inp
    
    * Update cp2k-pbe-template.inp
    
    * Update cp2k.py
    
    * Update cp2k-pbe-template.inp
    
    * Update test_cp2k.py
    
    * Update test_cp2k.py
    
    * Update environment-cuda117.yml
    
    * Update environment-cuda117.yml
    
    * Create job_control.txt
    
    * add chargemol files
    
    * Update cp2k.py
    
    * Update cp2k.py
    
    * Update cp2k.py
    
    * Update cp2k-pbe-template.inp
    
    * Update cp2k.py
    
    * Update cp2k-pbe-template.inp
    
    * Update cp2k-pbe-template.inp
    
    * Update cp2k.py
    
    * Update cp2k.py
    
    * Update job_control.txt
    
    * Update job_control.txt
    
    * Update cp2k.py
    
    * delete chargemol param files
    
    * Update environment-cuda117.yml
    
    * Update cp2k.py
    
    * Update cp2k.py
    
    * Update cp2k.py
    
    * Update cp2k.py
    
    * Update cp2k.py
    
    * Update cp2k.py
    
    * Update cp2k.py
    
    * Update cp2k.py
    
    * Update cp2k.py
    
    * Update cp2k.py
    
    * Update test_cp2k.py
    
    * Update cp2k-pbe-template.inp
    
    * Update cp2k.py
    
    * Update environment-aurora.yml
    
    * Update environment-cpu.yml
    
    * Update python-package-conda.yml
    
    * Update python-package-conda.yml
    
    * Update python-package-conda.yml
    
    * Update python-package-conda.yml
    
    * Update test_cp2k.py
    
    * Update python-package-conda.yml
    
    * Update python-package-conda.yml
    
    * Update python-package-conda.yml
    
    * Update python-package-conda.yml
    
    * Update python-package-conda.yml
    
    * Update python-package-conda.yml
    
    * Update test_cp2k.py
    
    * Update test_cp2k.py
    
    * Update test_cp2k.py
    
    * Update cp2k-pbe-template.inp
    
    * Separate chargemol into separate function
    
    ---------
    
    Co-authored-by: doublylinkedlist <williamyxl@gmail.com>

---
+++ mofa/simulation/cp2k.py
@@ -0,0 +1,118 @@
+"""Run computations backed by CP2K"""
+from dataclasses import dataclass
+from subprocess import run
+from pathlib import Path
+import sys
+import os
+
+from ase.calculators.cp2k import CP2K
+from ase import units
+
+from mofa.model import MOFRecord
+
+_file_dir = Path(__file__).parent / 'files'
+
+_cp2k_options = {
+    'pbe': {
+        'basis_set': 'DZVP-MOLOPT-SR-GTH',
+        'basis_set_file': "BASIS_MOLOPT",
+        'pseudo_potential': "GTH-PBE",
+        'potential_file': "GTH_POTENTIALS",
+        'cutoff': 600 * units.Ry
+    }
+}
+
+# Get the path to the CP2K atomic density guesses
+_atomic_density_folder_path = str(Path(sys.prefix) / "share" / "chargemol" / "atomic_densities")
+if not _atomic_density_folder_path.endswith("/"):
+    _atomic_density_folder_path = _atomic_density_folder_path + "/"
+
+
+def compute_partial_charges(cp2k_path: Path, threads: int | None = 2):
+    """Compute partial charges with DDEC
+
+    Args:
+        cp2k_path: Path to a CP2K computation which wrote a CUBE file
+        threads: Number of threads to use for chargemol
+    """
+
+    # Make a copy of the input file
+    with open(_file_dir / "chargemol" / "job_control.txt", "r") as rf:
+        write_str = rf.read().replace("$$$ATOMIC_DENSITY_DIR$$$", _atomic_density_folder_path)
+    with open(cp2k_path / "job_control.txt", "w") as wf:
+        wf.write(write_str)
+
+    # my local CP2k is not renaming the output automatically, so an extra renaming step is added here
+    if not (cp2k_path / "valence_density.cube").is_file():
+        cube_fname = list(cp2k_path.glob('*.cube'))
+        if len(cube_fname) != 1:
+            raise ValueError(f'Expected 1 cube file. Found {cube_fname}')
+        cube_fname[0].rename(cp2k_path / "valence_density.cube")
+
+    # chargemol uses all cores for OpenMP parallelism if no OMP_NUM_THREADS is set
+    with open(cp2k_path / 'chargemol.stdout', 'w') as fo, open(cp2k_path / 'chargemol.stderr', 'w') as fe:
+        if threads is not None:
+            env = os.environ.copy()
+            env['OMP_THREADS'] = str(threads)
+        else:
+            env = None
+        proc = run(["chargemol"], cwd=cp2k_path, env=env, stdout=fo, stderr=fe)
+        if proc.returncode != 0:
+            raise ValueError(f'Chargemol failed in {cp2k_path}')
+
+
+@dataclass
+class CP2KRunner:
+    """Interface for running pre-defined CP2K workflows"""
+
+    cp2k_invocation: str = 'cp2k_shell'
+    """Invocation used to run CP2K on this system"""
+
+    run_dir: Path = Path('cp2k-runs')
+    """Path in which to store CP2K files"""
+
+    def run_single_point(self, mof: MOFRecord, level: str = 'pbe') -> Path:
+        """Perform a single-point computation at a certain level
+
+        Args:
+            mof: Structure to be run
+            level: Name of the level of DFT computation to perform
+        Returns:
+            Path to the output files
+        """
+
+        # Get the template for this level of computation
+        template_file = _file_dir / f'cp2k-{level}-template.inp'
+        if not template_file.is_file():
+            raise ValueError(f'Template not found for {level}')
+
+        # Get the other settings
+        if level not in _cp2k_options:
+            raise ValueError(f'No presents for {level}')
+        options = _cp2k_options[level]
+
+        # Open then move to the output directory
+        # CP2K does not like long directory names in input files, so we move to the local directory
+        out_dir = self.run_dir / f'{mof.name}-single-{level}'
+        start_dir = Path().cwd()
+        out_dir.mkdir(parents=True, exist_ok=True)
+        (out_dir / 'cp2k.out').write_text('')  # Clear old content
+        os.chdir(out_dir)
+        try:
+            with CP2K(
+                    command=self.cp2k_invocation,
+                    directory=".",
+                    inp=template_file.read_text(),
+                    **options,
+            ) as calc:
+
+                # Run the calculation
+                atoms = mof.atoms
+                atoms.calc = calc
+                atoms.get_potential_energy()
+
+                # Write the
+                atoms.write('atoms.json')
+        finally:
+            os.chdir(start_dir)
+        return out_dir.absolute()
