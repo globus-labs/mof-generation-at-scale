0330a0a77b236ded54ad4088663bb884d6d91cf9
Logan Ward <ward.logan.t@gmail.com>
2024-04-05 09:10:41 -0400

    Move the processing function to mofa library

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -1,29 +1,31 @@
 """Test functions which validate a molecule given an XYZ file"""
-from ase import Atoms
 from ase.build import molecule
-from pytest import fixture, raises
+from pytest import fixture
 
-from mofa.assembly.validate import validate_xyz
-
-
-@fixture()
-def methane() -> Atoms:
-    return molecule('CH4')
+from mofa.assembly.validate import process_ligands
+from mofa.model import LigandDescription
+from mofa.utils.conversions import write_to_string
 
 
 @fixture()
-def bad_methane() -> Atoms:
+def methane() -> LigandDescription:
+    return LigandDescription(xyz=write_to_string(molecule('CH4'), 'xyz'), prompt_atoms=[[0]])
+
+
+@fixture()
+def bad_methane() -> LigandDescription:
     atoms = molecule('CH4')
     atoms.positions[3, 1] += 10
-    return atoms
+    return LigandDescription(xyz=write_to_string(atoms, 'xyz'), prompt_atoms=[[0]])
 
 
 def test_valid(methane):
-    smi = validate_xyz(methane)
-    assert smi == 'C'
+    valid_ligands, records = process_ligands([methane])
+    assert len(valid_ligands) == 1
+    assert records[0]['smiles'] == 'C'
 
 
 def test_disconnected(bad_methane):
-    with raises(ValueError) as exc:
-        validate_xyz(bad_methane)
-    assert 'Disconnected' in str(exc.value)
+    valid_ligands, records = process_ligands([bad_methane])
+    assert valid_ligands == []
+    assert not records[0]['valid']
