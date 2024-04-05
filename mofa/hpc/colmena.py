44d8f3ea0adb053d906419d452010ca4a61e661a
Logan Ward <ward.logan.t@gmail.com>
2024-04-05 19:02:25 -0400

    Send ProxyStore along with DiffLinker inf method

--- mofa/hpc/colmena.py
+++ mofa/hpc/colmena.py
@@ -1,12 +1,29 @@
 """Utilities specific to application using Colmena"""
-from typing import Any
+from typing import Any, Callable, Union, Generator, Iterable, Optional
 
 from colmena.models import Result
 from colmena.models.methods import PythonGeneratorMethod
+from colmena.queue import ColmenaQueues
+from proxystore.store import Store, register_store
 
 
 class DiffLinkerInference(PythonGeneratorMethod):
     """Subclass of the DiffLinker Python generator which submits linker post-processing as a new Task"""
+
+    def __init__(self,
+                 function: Callable[..., Union[Generator, Iterable]],
+                 name: Optional[str] = None,
+                 store_return_value: bool = False,
+                 streaming_queue: Optional[ColmenaQueues] = None,
+                 store: Optional[Store] = None) -> None:
+        super().__init__(function, name, store_return_value, streaming_queue)
+        self.store = store
+
+    def __setstate__(self, state):
+        super().__setstate__(state)
+
+        # Make sure the store is registered
+        register_store(self.store, exist_ok=True)
 
     def stream_result(self, y: Any, result: Result, start_time: float):
         """Submit a new task given the linkers"""
