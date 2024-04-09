8345762e87b7ea76f070ee0551f0deca38b6232f
doublylinkedlist <williamyxl@gmail.com>
2024-04-09 09:56:32 -0500

    Update raspa.py

--- mofa/simulation/raspa.py
+++ mofa/simulation/raspa.py
@@ -20,6 +20,197 @@ from mofa.model import MOFRecord
 
 logger = logging.getLogger(__name__)
 
+def read_lmp_sec_str2df(df_str, comment_char="#"):
+    df_str_list = list(filter(None, df_str.split("\n")))
+    df_str_list = [x.strip() for x in df_str_list]
+    rows, comments = list(map(list, zip(*[[pd.read_csv(io.StringIO(x.split(comment_char)[0].strip(
+    )), sep=r"\s+", header=None), x.split(comment_char)[1].strip()] for x in df_str_list])))
+    df = pd.concat(rows, axis=0).fillna("")
+    df[comment_char] = comment_char
+    df["comment"] = comments
+    return df.reset_index(drop=True)
+
+def write_pseudo_atoms_def(
+        raspa_path: str,
+        ff_style_dict: dict,
+        mass_df: pd.DataFrame,
+        atom_df: pd.DataFrame) -> (str):
+    """Use LAMMPS input files to write pseudo_atoms.def
+
+    Args:
+        raspa_path: output directory
+        mass_df: LAMMPS Masses table in Pandas format
+        atom_df: LAMMPS Atoms table in Pandas format
+    Returns:
+        raspa_file_name: written file name
+    """
+    mass_df.columns = ["type", "mass", "#", "comment"]
+    atom_df["mass"] = atom_df["type"].map(
+        dict(zip(mass_df["type"].to_list(), mass_df["mass"].to_list())))
+    atom_df["element"] = atom_df["type"].map(
+        dict(zip(mass_df["type"].to_list(), mass_df["comment"].to_list())))
+    _pseudo_atoms_df = pd.read_csv(
+        io.StringIO("""He     yes  He He 0  4.002602    0.0      0.0    1.0   1.0    0     0   relative    0
+C_co2  yes  C  C  0  12.0        0.6512   0.0    1.0   0.720  0     0   relative    0
+O_co2  yes  O  O  0  15.9994    -0.3256   0.0    1.0   0.68   0     0   relative    0
+O_o2   yes  O  O  0  15.9994    -0.112    0.0    1.0   0.7    0     0   relative    0
+O_com  no   O  -  0  0.0         0.224    0.0    1.0   0.7    0     0   relative    0
+N_n2   yes  N  N  0  14.00674   -0.405    0.0    1.0   0.7    0     0   relative    0
+N_com  no   N  -  0  0.0         0.810    0.0    1.0   0.7    0     0   relative    0
+Ar     yes  Ar Ar 0  39.948      0.0      0.0    1.0   0.7    0     0   relative    0
+CH4    yes  C  C  0  16.04246    0.0      0.0    1.0   1.00   0     0   relative    0
+CH3    yes  C  C  0  15.03452    0.0      0.0    1.0   1.00   0     0   relative    0
+CH2    yes  C  C  0  14.02658    0.0      0.0    1.0   1.00   0     0   relative    0
+CH     yes  C  C  0  13.01864    0.0      0.0    1.0   1.00   0     0   relative    0
+C      yes  C  C  0  12.0        0.0      0.0    1.0   1.00   0     0   relative    0
+"""),
+        sep=r"\s+",
+        header=None,
+        names=[
+            "#type",
+            "print",
+            "as",
+            "chem",
+            "oxidation",
+            "mass",
+            "charge",
+            "polarization",
+            "B-factor",
+            "radii",
+            "connectivity",
+            "anisotropic",
+            "anisotropic-type",
+            "tinker-type"])
+    pseudo_atoms_df_header_str = "#type      print   as    chem  oxidation   mass" + "        " + \
+                                 "charge   polarization B-factor radii  connectivity anisotropic anisotropic-type   tinker-type"
+    pseudo_atoms_df = atom_df[["comment", "element", "element", "mass", "q"]].copy(
+        deep=True).reset_index(drop=True)
+    pseudo_atoms_df.columns = ["#type", "as", "chem", "mass", "charge"]
+    pseudo_atoms_df["print"] = "yes"
+    pseudo_atoms_df["oxidation"] = "0"
+    pseudo_atoms_df["polarization"] = "0.0"
+    pseudo_atoms_df["B-factor"] = "1.0"
+    pseudo_atoms_df["radii"] = "1.00"
+    pseudo_atoms_df["connectivity"] = "0"
+    pseudo_atoms_df["anisotropic"] = "0"
+    pseudo_atoms_df["anisotropic-type"] = "relative"
+    pseudo_atoms_df["tinker-type"] = "0"
+    pseudo_atoms_df = pseudo_atoms_df[_pseudo_atoms_df.columns]
+    pseudo_atoms_df = pd.concat(
+        [pseudo_atoms_df, _pseudo_atoms_df], axis=0).reset_index(drop=True)
+    pseudo_atoms_str = "#number of pseudo atoms\n" + "%d" % len(pseudo_atoms_df) + "\n" + pseudo_atoms_df_header_str + "\n" + \
+                       "\n".join(["{:<10} {:<7} {:<5} {:<5} {:<11} {:<10} {:<9} {:<12} {:<8} {:<6} {:<12} {:<11} {:<18} {:<11}".format(
+                           pseudo_atoms_df.at[x, "#type"].strip(),
+                           pseudo_atoms_df.at[x, "print"],
+                           pseudo_atoms_df.at[x, "as"],
+                           pseudo_atoms_df.at[x, "chem"],
+                           pseudo_atoms_df.at[x, "oxidation"],
+                           pseudo_atoms_df.at[x, "mass"],
+                           "%1.4f" % pseudo_atoms_df.at[x, "charge"],
+                           pseudo_atoms_df.at[x, "polarization"],
+                           pseudo_atoms_df.at[x, "B-factor"],
+                           pseudo_atoms_df.at[x, "radii"],
+                           pseudo_atoms_df.at[x, "connectivity"],
+                           pseudo_atoms_df.at[x, "anisotropic"],
+                           pseudo_atoms_df.at[x, "anisotropic-type"],
+                           pseudo_atoms_df.at[x, "tinker-type"]) for x in pseudo_atoms_df.index])
+
+    with io.open(os.path.join(raspa_path, "pseudo_atoms.def"), "w", newline="\n") as wf:
+        wf.write(pseudo_atoms_str)
+    return "pseudo_atoms.def"
+
+def write_force_field_mixing_rules_def(
+        raspa_path: str,
+        ff_style_dict: dict,
+        pair_coeff_df: pd.DataFrame,
+        atom_df: pd.DataFrame) -> (str):
+    """Use LAMMPS input files to write pseudo_atoms.def
+
+    Args:
+        raspa_path: output directory
+        pair_coeff_df: LAMMPS Pair_Coeff table in Pandas format
+        atom_df: LAMMPS Atoms table in Pandas format
+    Returns:
+        raspa_file_name: written file name
+    """
+    NAvogadro = 6.02214076e23
+    kB = 1.380649e-23
+    kCal2Joule = 4184
+    lammps2raspa_energy = kCal2Joule / (NAvogadro * kB)
+    _mixing_df = read_lmp_sec_str2df(
+        """He             lennard-jones    10.9      2.64""" +
+        "         " + """// J.O. Hirschfelder et al., Molecular Theory of Gases and Liquids, Wiley, New York, 1954, p. 1114.
+O_co2          lennard-jones    85.671    3.017        // A. Garcia-Sanchez et al., J. Phys. Chem. C 2009, 113, 8814-8820.
+C_co2          lennard-jones    29.933    2.745        // idem
+N_n2           lennard-jones    38.298    3.306        // A. Martin-Calvo et al. , Phys. Chem. Chem. Phys. 2011, 13, 11165-11174.
+N_com          none                                    // idem
+O_o2           lennard-jones    53.023    3.045        // A. Martin-Calvo et al. , Phys. Chem. Chem. Phys. 2011, 13, 11165-11174.
+O_com          none                                    //
+Ar             lennard-jones   124.070    3.38         // A. Martin-Calvo et al. , Phys. Chem. Chem. Phys. 2011, 13, 11165-11174.
+CH4            lennard-jones    158.5     3.72         // M. G. Martin et al., J. Chem. Phys. 2001, 114, 7174-7181.
+CH3            lennard-jones    108.0     3.76         // D. Dubbeldam et al., J. Phys. Chem. B, 108(33), 12301-12313
+CH2            lennard-jones    56.0      3.96         // idem
+CH             lennard-jones    17.0      4.67         // idem
+C              lennard-jones     0.8      6.38         // idem
+""", comment_char="//")
+    _mixing_df.columns = [
+        "atom",
+        "type",
+        "eps(K)",
+        "sig(Ang)",
+        "//",
+        "comment"]
+
+    pair_coeff_df.columns = [
+        "type",
+        "eps(kCal/mol)",
+        "sig(Ang)",
+        "#",
+        "comment"]
+    pair_coeff_df["eps(K)"] = [
+        "%.4f" %
+        x for x in (
+            pair_coeff_df["eps(kCal/mol)"] *
+            lammps2raspa_energy)]
+    pair_coeff_df["sig(Ang)"] = ["%.4f" %
+                                 x for x in pair_coeff_df["sig(Ang)"]]
+    atom_df["eps(K)"] = atom_df["type"].map(
+        dict(zip(pair_coeff_df["type"], pair_coeff_df["eps(K)"])))
+    atom_df["sig(Ang)"] = atom_df["type"].map(
+        dict(zip(pair_coeff_df["type"], pair_coeff_df["sig(Ang)"])))
+    mixing_df = atom_df[["comment", "eps(K)", "sig(Ang)"]].copy(
+        deep=True).reset_index(drop=True)
+    mixing_df.columns = ["atom", "eps(K)", "sig(Ang)"]
+    mixing_df["type"] = "lennard-jones"
+    mixing_df["//"] = "//"
+    mixing_df["comment"] = "UFF4MOF"
+    mixing_df = pd.concat([mixing_df, _mixing_df],
+                          axis=0).reset_index(drop=True)
+    mixing_str = "\n".join(["{:<14} {:<16} {:<9} {:<12} {:<3} {:<100}".format(
+        mixing_df.at[x, "atom"].strip(),
+        mixing_df.at[x, "type"].strip(),
+        mixing_df.at[x, "eps(K)"],
+        mixing_df.at[x, "sig(Ang)"],
+        mixing_df.at[x, "//"].strip(),
+        mixing_df.at[x, "comment"].strip(),
+    ) for x in mixing_df.index])
+    mixing_method = "Lorentz-Berthelot"
+    if "geometric" in ff_style_dict["pair_modify"]:
+        mixing_method = "Jorgensen"
+    force_field_mixing_rules_str = """# general rule for shifted vs truncated
+shifted
+# general rule tailcorrections
+yes
+# number of defined interactions
+""" + "%d" % len(mixing_df) + """
+# type interaction
+""" + mixing_str + """
+# general mixing rule for Lennard-Jones
+""" + mixing_method + """
+"""
+    with io.open(os.path.join(raspa_path, "force_field_mixing_rules.def"), "w", newline="\n") as wf:
+        wf.write(force_field_mixing_rules_str)
+    return "force_field_mixing_rules.def"
 
 class RASPARunner:
     """Interface for running pre-defined RASPA workflows
@@ -134,14 +325,6 @@ _atom_site_charge
             "Masses")[1].split("Pair Coeffs")[0].strip())
         pair_coeff_df = read_lmp_sec_str2df(read_str.split(
             "Pair Coeffs")[1].split("Bond Coeffs")[0].strip())
-        bond_coeff_df = read_lmp_sec_str2df(read_str.split(
-            "Bond Coeffs")[1].split("Angle Coeffs")[0].strip())
-        angle_coeff_df = read_lmp_sec_str2df(read_str.split(
-            "Angle Coeffs")[1].split("Dihedral Coeffs")[0].strip())
-        dihedral_coeff_df = read_lmp_sec_str2df(read_str.split("Dihedral Coeffs")[
-            1].split("Improper Coeffs")[0].strip())
-        improper_coeff_df = read_lmp_sec_str2df(
-            read_str.split("Improper Coeffs")[1].split("Atoms")[0].strip())
         cifbox = read_str.split("Atoms")[1].split("$$$atoms$$$")[0].strip()
         atom_df = read_lmp_sec_str2df(read_str.split("$$$atoms$$$")[
             1].split("Bonds")[0].strip())
@@ -154,17 +337,13 @@ _atom_site_charge
                 "comment", "fx", "fy", "fz"])
         atom_df = pd.concat([atom_df[['id', 'mol', 'type', 'q', 'x', 'y', 'z', '#']].reset_index(
             drop=True), _atom_df.reset_index(drop=True)], axis=1)
-        bond_df = read_lmp_sec_str2df(
-            read_str.split("Bonds")[1].split("Angles")[0].strip())
-        angle_df = read_lmp_sec_str2df(read_str.split(
-            "Angles")[1].split("Dihedrals")[0].strip())
-        dihedral_df = read_lmp_sec_str2df(read_str.split(
-            "Dihedrals")[1].split("Impropers")[0].strip())
-        improper_df = read_lmp_sec_str2df(
-            read_str.split("Impropers")[1].strip())
 
+    atom_fname = write_pseudo_atoms_def(
+        raspa_path, ff_style_dict, mass_df, atom_df)
+    mix_fname = write_force_field_mixing_rules_def(
+        raspa_path, ff_style_dict, pair_coeff_df, atom_df)
 
-    def run_He_void_single(self, run_name: str, atoms: ase.Atoms, timesteps: int, report_frequency: int, mof_ase_atoms) -> str:
+    def run_He_void_single(self, mof_ase_atoms: ase.Atoms, run_name: str, timesteps: int, report_frequency: int, temperature_K: float) -> float:
         """Use cif2lammps to assign force field to a single MOF and generate input files for raspa simulation
 
         Args:
@@ -202,31 +381,42 @@ flexible
 0
 """)
 
-        
-        
         # He void fraction input
         with open(Path(raspa_path) / "simulation.input", "w") as wf:
             wf.write("""SimulationType                       MonteCarlo
-NumberOfCycles                       40000
-PrintEvery                           1000
-PrintPropertiesEvery                 1000
+NumberOfCycles                       """ + str(timesteps) + """
+PrintEvery                           """ + str(report_frequency) + """
+PrintPropertiesEvery                 """ + str(report_frequency) + """
 
 Forcefield                           local
 
 Framework 0
 FrameworkName mof
 UnitCells 2 2 2
-ExternalTemperature 300.0
+ExternalTemperature """ + "%.2f" % temperature_K + """
 
 Component 0 MoleculeName             helium
             MoleculeDefinition       local
             WidomProbability         1.0
             CreateNumberOfMolecules  0
 """)
+        # run He void calcultion
+        with open(raspa_path / 'stdout_he_void.raspa', 'w') as fp, open(raspa_path / 'stderr_he_void.raspa', 'w') as fe:
+            env = None
+            if self.raspa_environ is not None:
+                env = os.environ.copy()
+                env.update(self.raspa_environ)
+            return run(list(self.raspa_command), cwd=raspa_path, stdout=fp, stderr=fe, env=env)
 
-        
-
-        return raspa_path
+        # parse output
+        outdir = os.path.join(raspa_path, "Output")
+        outdir = os.path.join(outdir, os.listdir(outdir)[0])
+        outfile = os.path.join(outdir, [x for x in os.listdir(outdir) if "output_mof" in x][0])
+        outstr = None
+        with open(outfile, "r", newline="\\n") as rf:
+            outstr = rf.read()
+        He_Void_Faction = float(outstr.split("[helium] Average Widom Rosenbluth-weight:")[1].split("+/-")[0])
+        return He_Void_Faction
 
     def run_gcmc(self, mof: MOFRecord, timesteps: int, report_frequency: int) -> list[ase.Atoms]:
         """Run a molecular dynamics trajectory
@@ -290,10 +480,4 @@ rigid
             Log from the completed process
         """
 
-        raspa_path = Path(raspa_path)
-        with open(raspa_path / 'stdout.raspa', 'w') as fp, open(raspa_path / 'stderr.raspa', 'w') as fe:
-            env = None
-            if self.raspa_environ is not None:
-                env = os.environ.copy()
-                env.update(self.raspa_environ)
-            return run(list(self.raspa_command), cwd=raspa_path, stdout=fp, stderr=fe, env=env)
+        
