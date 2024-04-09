be0500a91eedb84f477b2fab02efe19a580e4a98
doublylinkedlist <williamyxl@gmail.com>
2024-04-09 15:06:26 -0500

    Update test_raspa.py

--- tests/simulation/test_raspa.py
+++ tests/simulation/test_raspa.py
@@ -1,13 +1,13 @@
 from pytest import mark
 from mofa.simulation.raspa import RASPARunner
 from ase.io import read
-
+from pathlib import Path
 
 @mark.parametrize('extxyz_name', ['test-zn'])
 def test_raspa_runner(extxyz_name, cif_dir, tmpdir):
     # Make a RASPA simulator that reads and writes to a temporary directory
     runner = RASPARunner()
-    test_file = cif_dir / f'{extxyz_name}.extxyz'
+    test_file = Path(cif_dir) / f'{extxyz_name}.extxyz'
     ase_atoms = read(test_file, format="extxyz")
     ads_mean, ads_std = runner.run_GCMC_single(ase_atoms, run_name="some_random_string", timesteps=200, report_frequency=1)
     assert isinstance(ads_mean, float)
