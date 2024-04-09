b56678143e7aed0af53672d4784c0fdc6ca3a3c7
doublylinkedlist <williamyxl@gmail.com>
2024-04-09 13:14:45 -0500

    Update test_raspa.py

--- tests/simulation/test_raspa.py
+++ tests/simulation/test_raspa.py
@@ -8,7 +8,7 @@ def test_raspa_runner(extxyz_name, cif_dir, tmpdir):
     # Make a RASPA simulator that reads and writes to a temporary directory
     runner = RASPARunner()
     test_file = cif_dir / f'{extxyz_name}.extxyz'
-    ase_atoms = read(test_file, fmt="extxyz")
+    ase_atoms = read(test_file, format="extxyz")
     ads_mean, ads_std = runner.run_GCMC_single(ase_atoms, run_name="Zn-MOF-74", timesteps=200, report_frequency=1)
     assert isinstance(ads_mean, float)
     assert isinstance(ads_std, float)
