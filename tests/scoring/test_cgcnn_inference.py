6ac3232cbf635aa33d274cc6a659845bef91426f
doublylinkedlist <williamyxl@gmail.com>
2024-04-10 15:33:26 -0500

    Update test_cgcnn_inference.py

--- tests/scoring/test_cgcnn_inference.py
+++ tests/scoring/test_cgcnn_inference.py
@@ -5,8 +5,8 @@ from ase.io import read
 from pathlib import Path
 
 
-@mark.parametrize('extxyz_name', ['test-zn'])
-def test_run_cgcnn_pred_wrapper_serial(extxyz_name, cif_dir, tmpdir):
+def test_run_cgcnn_pred_wrapper_serial():
+    cif_dir = Path(__file__).parent.resolve() / '..' / "simulation" / "cif_files"
     my_ase_mofs = [read(Path(cif_dir) / x, format="cif") for x in os.listdir(cif_dir) if x.endswith(".cif")]
     pred, std = run_cgcnn_pred_wrapper_serial(my_ase_mofs, manual_batch_size=7, ncpus_to_load_data=1)
     assert len(pred) == len(my_ase_mofs)
