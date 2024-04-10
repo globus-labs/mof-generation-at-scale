86c7df01e1bc29b77ddbf60ae6cf6b06cc9c3d30
doublylinkedlist <williamyxl@gmail.com>
2024-04-10 14:09:38 -0500

    Update test_cgcnn_inference.py

--- tests/scoring/test_cgcnn_inference.py
+++ tests/scoring/test_cgcnn_inference.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 @mark.parametrize('extxyz_name', ['test-zn'])
 def test_run_cgcnn_pred_wrapper_serial(extxyz_name, cif_dir, tmpdir):
-    my_ase_mofs = [read(Path(datadir) / x, format="cif") for x in os.listdir(datadir) if x.endswith(".cif")]
+    my_ase_mofs = [read(Path(cif_dir) / x, format="cif") for x in os.listdir(cif_dir) if x.endswith(".cif")]
     pred, std = run_cgcnn_pred_wrapper_serial(my_ase_mofs, manual_batch_size=7, ncpus_to_load_data=1)
     assert len(pred) == len(my_ase_mofs)
     assert len(std) == len(my_ase_mofs)
