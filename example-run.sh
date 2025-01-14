3b1f7e69c3155a0cf6254bfbee35dc45b4255c0d
Logan Ward <WardLT@users.noreply.github.com>
2025-01-14 16:39:19 -0500

    Remove serial versions (#144)

--- example-run.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-python run_serial_workflow.py \
-      --node-path input-files/zn-paddle-pillar/node.json \
-      --generator-path input-files/zn-paddle-pillar/geom_difflinker.ckpt \
-      --ligand-templates input-files/zn-paddle-pillar/template_*_prompt.yml \
-      --num-samples 64 \
-      --num-to-assemble 4 \
-      --torch-device cuda
