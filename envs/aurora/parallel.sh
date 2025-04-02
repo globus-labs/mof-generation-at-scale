ab8dea29a60e3f9c00fc013fb55e68a1b0072a99
Logan Ward <ward.logan.t@gmail.com>
2025-04-02 19:48:42 +0000

    Use nohup when launching workers to free parallel deployer

--- envs/aurora/parallel.sh
+++ envs/aurora/parallel.sh
@@ -22,11 +22,9 @@ export LD_LIBRARY_PATH=$FPATH/intel_extension_for_pytorch/lib:$LD_LIBRARY_PATH
 # Put RASPA2 on the path
 export PATH=$PATH:`realpath conda-env/bin/`
 
-$exe $args
+nohup $exe $args &
 END
 )
 
 # Execute with GNU parallel
-#  TODO (wardlt): Consider using nohup to allow the SSH to hangup after launching
-nnodes=`wc -l $sshfile`
-parallel -j $nnodes --env _ --nonall --sshloginfile $sshfile "$command"
+parallel --env _ --nonall --sshloginfile $sshfile "$command"
