3a8999a1e2f8f7cd19e2f2c5bd8590af9dc6cf79
Azton Wells <azton.wells@gmail.com>
2025-04-04 15:28:36 +0000

    put changes for aurora install in build-aurora

--- envs/aurora/build-aurora.sh
+++ envs/aurora/build-aurora.sh
@@ -6,6 +6,9 @@ python3 -m venv ./venv --system-site-packages
 source ./venv/bin/activate
 
 # Install the MOFA stuff
+pip install -e .
+# for aurora, take out the pypi install
+pip uninstall pytorch-lightning -y
+# put in the one with xpu support
 export PACKAGE_NAME=pytorch
 pip install git+https://github.com/azton/lightning.git
-pip install -e .
