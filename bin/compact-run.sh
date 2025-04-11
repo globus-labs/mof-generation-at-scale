302e244abf1ef575a2e95900a3bc10a9173c1e34
Logan Ward <ward.logan.t@gmail.com>
2025-04-11 02:13:12 +0000

    Make a script for compressing run files

---
+++ bin/compact-run.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+
+# Compress cubes and log files
+find $1 -name "*.cube" | xargs -P 4 -n 1 bzip2 -9 --verbose
+find $1/logs -name "*.log" | xargs -P 4 -n 1 bzip2 -9 --verbose
+find $1/logs -name "*.csv" | xargs -P 4 -n 1 bzip2 -9 --verbose
