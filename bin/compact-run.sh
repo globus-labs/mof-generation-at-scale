596342908f98455b0a8381fef0b1413dd2616ac9
Logan Ward <ward.logan.t@gmail.com>
2025-04-14 11:32:07 +0000

    Also compact results files

--- bin/compact-run.sh
+++ bin/compact-run.sh
@@ -2,6 +2,8 @@
 
 
 # Compress cubes and log files
+bzip2 -9 --verbose $1/all_ligands.csv
+ls "$/*-results.json" | xargs -P 4 -n 1 bzip2 -9 --verbose
 find $1 -name "*.cube" | xargs -P 4 -n 1 bzip2 -9 --verbose
 find $1/logs -name "*.log" | xargs -P 4 -n 1 bzip2 -9 --verbose
 find $1/logs -name "*.csv" | xargs -P 4 -n 1 bzip2 -9 --verbose
