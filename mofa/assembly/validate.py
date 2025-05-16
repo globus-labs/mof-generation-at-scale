6aab6471a54ba40e498128aba16fd2ce1f0e5637
Logan Ward <ward.logan.t@gmail.com>
2025-05-16 14:05:02 -0400

    Fix the validation tests

--- mofa/assembly/validate.py
+++ mofa/assembly/validate.py
@@ -6,6 +6,57 @@ from mofa.utils.xyz import xyz_to_mol
 from mofa.utils.difflinker_sample_and_analyze import DiffLinkerOutput
 
 
+def check_ligand(ligand: LigandDescription) -> tuple[LigandDescription, dict]:
+    """Check whether an individual ligand description is satifactory
+
+    Args:
+        ligand: Ligand to be evaluated
+    Returns:
+        - Modified description
+        - Record describing it, a dict which contains:
+            - anchor_type: The type of the anchor for the ligand
+            - name: Name assigned by generator
+            - smiles: SMILES string (if detectible)
+            - xyz: Geometry
+            - prompt_atoms: Indices of the atoms used in the prompt
+            - valid: Whether the molecule was valid
+    """
+    # Store the ligand information for debugging purposes
+    record = {"anchor_type": ligand.anchor_type,
+              "name": ligand.name,
+              "smiles": None,
+              "xyz": ligand.xyz,
+              "prompt_atoms": ligand.prompt_atoms,
+              "valid": False}
+
+    # Try constrained optimization on the ligand
+    try:
+        ligand.full_ligand_optimization()
+    except (ValueError, AttributeError,):
+        return ligand, record
+
+    # Parse each new ligand, determine whether it is a single molecule
+    try:
+        mol = xyz_to_mol(ligand.xyz)
+    except (ValueError,):
+        return ligand, record
+
+    # Store the smiles string
+    mol = Chem.RemoveHs(mol)
+    smiles = Chem.MolToSmiles(mol)
+    record['smiles'] = smiles
+
+    if len(Chem.GetMolFrags(mol)) > 1:
+        return ligand, record
+
+    # If passes, save the SMILES string and store the molecules
+    ligand.smiles = Chem.MolToSmiles(mol)
+
+    # Update the record, add to ligand queue and prepare it for writing to disk
+    record['valid'] = True
+    return ligand, record
+
+
 def process_ligands(ligands: list[DiffLinkerOutput]) -> tuple[list[LigandDescription], list[dict]]:
     """Assess whether a ligand is valid and prepare it for the next step
 
@@ -23,39 +74,5 @@ def process_ligands(ligands: list[DiffLinkerOutput]) -> tuple[list[LigandDescrip
         # Generate the description from the template
         ligand = template.create_description(symbols, coords)
 
-        # Store the ligand information for debugging purposes
-        record = {"anchor_type": ligand.anchor_type,
-                  "name": ligand.name,
-                  "smiles": None,
-                  "xyz": ligand.xyz,
-                  "prompt_atoms": ligand.prompt_atoms,
-                  "valid": False}
-        all_records.append(record)  # Record is still editable even after added to list
-
-        # Try constrained optimization on the ligand
-        try:
-            ligand.full_ligand_optimization()
-        except (ValueError, AttributeError,):
-            continue
-
-        # Parse each new ligand, determine whether it is a single molecule
-        try:
-            mol = xyz_to_mol(ligand.xyz)
-        except (ValueError,):
-            continue
-
-        # Store the smiles string
-        mol = Chem.RemoveHs(mol)
-        smiles = Chem.MolToSmiles(mol)
-        record['smiles'] = smiles
-
-        if len(Chem.GetMolFrags(mol)) > 1:
-            continue
-
-        # If passes, save the SMILES string and store the molecules
-        ligand.smiles = Chem.MolToSmiles(mol)
-
-        # Update the record, add to ligand queue and prepare it for writing to disk
-        record['valid'] = True
         valid_ligands.append(ligand)
     return valid_ligands, all_records
--- tests/test_validate.py
+++ tests/test_validate.py
@@ -2,7 +2,7 @@
 from ase.build import molecule
 from pytest import fixture
 
-from mofa.assembly.validate import process_ligands
+from mofa.assembly.validate import check_ligand
 from mofa.model import LigandDescription
 from mofa.utils.conversions import write_to_string
 
@@ -20,12 +20,10 @@ def bad_ethane() -> LigandDescription:  # don't use broken H to test disconnecti
 
 
 def test_valid(methane):
-    valid_ligands, records = process_ligands([methane])
-    assert len(valid_ligands) == 1
-    assert records[0]['smiles'] == 'C'
+    _, record = check_ligand(methane)
+    assert record['smiles'] == 'C'
 
 
 def test_disconnected(bad_ethane):
-    valid_ligands, records = process_ligands([bad_ethane])
-    assert valid_ligands == []
-    assert not records[0]['valid']
+    _, record = check_ligand(bad_ethane)
+    assert not record['valid']
